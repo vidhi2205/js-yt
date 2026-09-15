82552f48f1853ec6bcb866f0514e251c527dbf84
vidhi2205 <vidhiagarwalmay@gmail.com>
2026-09-15 11:19:53 +0000

    added datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we are using node js and not browser
+
+console.log(3+3)
+console.log("vidhi")
+
+let name="vidhi"
+let age=19
+let isLoggedIn=false
+
+// number=>2^53
+// bigint
+// string=>""
+// boolean=> true/false
+// null=>standalone value
+// undefined
+// symbol=>unique
+
+
+// object
+
+console.log(typeof null) // gives object
+console.log(typeof undefined) // undefined
