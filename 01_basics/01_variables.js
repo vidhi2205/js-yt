ad5e2d70ca2becbd09011cbff9f8b9f02ee6e88a
vidhi2205 <vidhiagarwalmay@gmail.com>
2026-09-15 11:10:56 +0000

    added var.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=144553
+let accountEmail="vidhi@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2
+
+accountEmail="vidhicom"
+accountPassword="12431243"
+accountCity="india"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope 
+and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
