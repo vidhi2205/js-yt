12644dc07c056c3c2e3aa0bd1514a69f12bf6b69
vidhi2205 <vidhiagarwalmay@gmail.com>
2026-09-15 11:32:47 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score=true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33"=>33
+// "33ab"=>NaN
+// true=>1; false=>0
+
+let isLoggedIn="vidhi"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=>true;0=>false
+// "vidhi"=>true;""=>false
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
